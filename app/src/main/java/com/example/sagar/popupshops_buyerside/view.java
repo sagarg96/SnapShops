2fdb6f0fea6d46894ba8619709e911bf9fba71c5
ish1212 <ish_handa@outlook.com>
2017-11-04 17:59:14 +0800

    adding edit text field

--- app/src/main/java/com/example/sagar/popupshops_buyerside/view.java
+++ app/src/main/java/com/example/sagar/popupshops_buyerside/view.java
@@ -2,9 +2,13 @@ package com.example.sagar.popupshops_buyerside;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Vikram on 03-Nov-17.
@@ -20,6 +24,8 @@ import android.widget.Button;
             Button listview = (Button)findViewById(R.id.lis);
             Button addtolist = (Button)findViewById(R.id.ad);
             final Button locate = (Button)findViewById(R.id.loc);
+            EditText TextHandler = (EditText)findViewById(R.id.editer);
+            TextHandler.setOnKeyListener(new TextHandler());
 
             listview.setOnClickListener(new Button.OnClickListener(){
                 public void onClick(View view){
@@ -42,5 +48,21 @@ import android.widget.Button;
                     }
                 }
             });
+        }
+
+        public class TextHandler implements View.OnKeyListener
+        {
+            @Override
+            public boolean onKey(View view, int KeyCode, KeyEvent keyEvent) {
+                int viewID = view.getId();
+                EditText input_text = (EditText)findViewById(viewID);
+                String the_text = input_text.getText().toString();
+
+                if (KeyCode == KeyEvent.KEYCODE_ENTER)
+                {
+                    Toast.makeText(view.this,"Profile Updated" + the_text,Toast.LENGTH_LONG);
+                }
+                return false;
+            }
         }
     }
