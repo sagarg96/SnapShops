45565381827aed4574f55fc59b3c5ae3c9d864bf
fawad <fawadmasooddesmukh@gmail.com>
2017-11-13 21:20:51 +0800

    completed dynamic field update

--- app/src/main/java/com/example/sagar/popupshops_buyerside/Shop/vendor_dashboard.java
+++ app/src/main/java/com/example/sagar/popupshops_buyerside/Shop/vendor_dashboard.java
@@ -1,10 +1,13 @@
 package com.example.sagar.popupshops_buyerside.Shop;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -19,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
@@ -72,6 +76,24 @@ public class vendor_dashboard extends AppCompatActivity {
         shopDescription.setOnKeyListener(new descriptionTextHandler());
         shopName = (EditText) findViewById(R.id.shopName);
         shopName.setOnKeyListener(new nameTextHandler());
+
+        shopDescription.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+
+        shopName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
 
 
         viewItemList.setOnClickListener(new Button.OnClickListener() {
@@ -155,15 +177,52 @@ public class vendor_dashboard extends AppCompatActivity {
         FirebaseUtils.getUsersRef().child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(FirebaseEndpoint.USERS.SHOPS).setValue(shopList);
     }
 
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
     public class descriptionTextHandler implements View.OnKeyListener {
         @Override
         public boolean onKey(View view, int KeyCode, KeyEvent keyEvent) {
             int viewID = view.getId();
             EditText input_text = (EditText) findViewById(viewID);
-            String the_text = input_text.getText().toString();
+            final String the_text = input_text.getText().toString();
 
             if (KeyCode == KeyEvent.KEYCODE_ENTER) {
                 Toast.makeText(vendor_dashboard.this, "Description Updated " + the_text, Toast.LENGTH_LONG).show();
+                //press enter to update shop description
+
+                Query shopExistQuery = FirebaseUtils.getUsersRef().child(FirebaseUtils.getCurrentUser().getUid()).child(FirebaseEndpoint.USERS.SHOPS);
+                shopExistQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        boolean notDone = true;
+                        if (dataSnapshot.exists()) {
+                            //if shop exists go to shop dashboard
+                            Log.w("here", "shop exists " + dataSnapshot.getValue().toString());
+//                            DatabaseReference databaseReference = FirebaseUtils.getCurrentUserShop().child(FirebaseEndpoint.SHOPS.DESCRIPTION).push();
+//                            databaseReference.setValue(the_text);
+                            if (notDone) {
+                                String val = dataSnapshot.getValue().toString();
+                                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(FirebaseEndpoint.ROOTS.SHOPS).child(val.substring(1, val.length() - 1)).child(FirebaseEndpoint.SHOPS.DESCRIPTION);
+                                databaseReference.setValue(the_text);
+                                notDone = false;
+                            }
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Getting Post failed, log a message
+                        Log.w("here", "loadPost:onCancelled", databaseError.toException());
+                        // ...
+
+                    }
+                });
+
             }
             return false;
         }
@@ -174,10 +233,39 @@ public class vendor_dashboard extends AppCompatActivity {
         public boolean onKey(View view, int KeyCode, KeyEvent keyEvent) {
             int viewID = view.getId();
             EditText input_text = (EditText) findViewById(viewID);
-            String the_text = input_text.getText().toString();
+            final String the_text = input_text.getText().toString();
 
             if (KeyCode == KeyEvent.KEYCODE_ENTER) {
                 Toast.makeText(vendor_dashboard.this, "Shop Name Updated " + the_text, Toast.LENGTH_LONG).show();
+                Query shopExistQuery = FirebaseUtils.getUsersRef().child(FirebaseUtils.getCurrentUser().getUid()).child(FirebaseEndpoint.USERS.SHOPS);
+                shopExistQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        boolean notDone = true;
+                        if (dataSnapshot.exists()) {
+                            Log.w("here", "shop exists " + dataSnapshot.getValue().toString());
+                            if (notDone) {
+                                String val = dataSnapshot.getValue().toString();
+                                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(FirebaseEndpoint.ROOTS.SHOPS).child(val.substring(1, val.length() - 1)).child(FirebaseEndpoint.SHOPS.SHOPNAME);
+                                databaseReference.setValue(the_text);
+                                notDone = false;
+                            }
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Getting Post failed, log a message
+                        Log.w("here", "loadPost:onCancelled", databaseError.toException());
+                        // ...
+
+                    }
+                });
+
+
+
             }
             return false;
         }
