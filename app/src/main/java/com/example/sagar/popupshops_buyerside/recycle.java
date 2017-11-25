a73f54ee4a1b38d83498456158f95a0e8e706710
ish1212 <ish_handa@outlook.com>
2017-11-26 01:13:01 +0800

    UI update 0.6

--- app/src/main/java/com/example/sagar/popupshops_buyerside/recycle.java
+++ app/src/main/java/com/example/sagar/popupshops_buyerside/recycle.java
@@ -14,6 +14,7 @@ public class recycle extends vendor_dashboard {
     private List<list> lists;
     private RecyclerView rv;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
